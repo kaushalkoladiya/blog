cd5200ab7e7a1208fac6b5a783c45f8167e6ed40
Kaushal <50080954+kausHal789@users.noreply.github.com>
2020-04-24 12:21:50 +0530

    Add res
    Browser can send first OPTIONS reqest. So I have to response in status 200.

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	if (req.method == 'OPTIONS') {
-		return;
+		return res.sendStatus(200);
 	}
 	next();
 });
